b352f525dc20fb0149650631b0e1f184ff6cb1d0
Johan Félisaz <johan.felisaz@protonmail.com>
2020-12-01 10:52:12 +0100

    day 1 part 2

--- day1.lisp
+++ day1.lisp
@@ -14,5 +14,19 @@
       (setf (gethash x hash) x))
     ;; Find the pair
     (loop for x being the hash-key of hash do
-      (if (gethash (- 2020 x) hash)
+      (when (gethash (- 2020 x) hash)
 	  (return (* x (- 2020 x)))))))
+
+(defun find-three-entries ()
+  (let ((entries (load-data "data/day1.dat"))
+	(hash (make-hash-table)))
+    ;; Fill the hash table
+    (loop for x in entries do
+      (setf (gethash x hash) x))
+    ;; Find the pair
+    (loop for x being the hash-key of hash do
+      (loop for y being the hash-key of hash
+	    ;; when (<= (+ x y) 2020)
+	    do
+	       (when (gethash (- 2020 x y) hash)
+		 (return-from find-three-entries (* x y (- 2020 x y))))))))
